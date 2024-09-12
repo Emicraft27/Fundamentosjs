27846bf24ba5dec7c51e81a2a79c49e657a5a9bb
Emicraft27 <emiliovallejo456@gmail.com>
2024-09-12 13:57:23 -0600

    03 Variables let y const

---
+++ concepts/operators.js
@@ -0,0 +1,38 @@
+// Operadores Matematicos
+// = y -
+// * y /
+// % y ** Modulo y potencia
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+
+//Opreadores de Asignacion
+let a = 10;
+a += 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+console.log(a);
+
+
+// Operadores de Comparacion
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+// Operadores Logicos
+console.log(true && true); // and
+console.log(true || false); // or
+console.log(!true); // not
