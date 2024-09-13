638a58cd797c3044efeeb9a400efd5a7be07ce35
Emicraft27 <emiliovallejo456@gmail.com>
2024-09-13 13:39:35 -0600

    04 Strings

---
+++ concepts/strings.js
@@ -0,0 +1,26 @@
+// Hi! i'm Emilio and I'm from Huamantla
+
+const name = "Emilio";
+const city = 'Huamantla';
+
+console.log("Hi! I'm " + name + " \n i'm from " + city);
+
+console.log('Hi! I\'m ' + ' \n  I\'m from ' + city)
+
+
+//Template literals
+console.log(`Hi! I'm ${ name }
+i'm from ${ city }`);
+
+
+const message = `Hi!' I'm ${ name } i'm fron ${ city }`;
+
+
+console.log(message. length);
+console.log(message. toUpperCase());
+console.log(message. toLowerCase());
+console.log(message. includes('from'));
+console.log(message. startsWith('Hi!'));
+console.log(message. indexOf("Emilio"));
+console.log(message. split(''));
+console.log("=😍^.^=😂". repeat(5));
